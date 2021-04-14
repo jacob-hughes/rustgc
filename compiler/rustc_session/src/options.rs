0c0f5dbf5981595b221ecf565bce0dc49c1f31a6
Jake Hughes <jh@jakehughes.uk>
2021-04-14 01:34:25 +0100

    Put precise marking optimisation behind a feature gate
    This lets us switch it on or off for debugging using:
        RUSTFLAGS="-C gc_precise_marking=true"
    
    It is off by default

--- compiler/rustc_session/src/options.rs
+++ compiler/rustc_session/src/options.rs
@@ -1246,6 +1246,8 @@ options! {
     #[rustc_lint_opt_deny_field_access("use `Session::must_emit_unwind_tables` instead of this field")]
     force_unwind_tables: Option<bool> = (None, parse_opt_bool, [TRACKED],
         "force use of unwind tables"),
+    gc_precise_marking: bool = (false, parse_bool, [TRACKED],
+        "allocate objects with support for precise marking"),
     incremental: Option<String> = (None, parse_opt_string, [UNTRACKED],
         "enable incremental compilation"),
     inline_threshold: Option<u32> = (None, parse_opt_number, [TRACKED],
