e20cc387744f91e05c0800ac695a9b240e2d0181
Jake Hughes <jh@jakehughes.uk>
2023-11-21 13:32:17 +0000

    Use GC_memalign for larger allocations
    When a layout's alignment is larger than its size, we must allocate
    using Boehm's `memalign` function instead of `malloc` in order to mirror
    the existing behaviour in rustc.
    
    This is checked in rustc with various assertions on pointer read and
    write intrinsics, and was discovered when they failed with large
    alloctions in lrlex using a debug build of alloy.

---
+++ library/alloc/tests/gc.rs
@@ -0,0 +1,47 @@
+use std::alloc::GlobalAlloc;
+use std::alloc::Layout;
+use std::gc::GcAllocator;
+
+#[allow(dead_code)]
+#[repr(align(1024))]
+struct S(u8);
+
+#[allow(dead_code)]
+#[repr(align(16))]
+struct T(usize, usize, usize, usize);
+
+#[test]
+fn large_alignment() {
+    let x = Box::new_in(S(123), GcAllocator);
+    let ptr = Box::into_raw(x);
+    assert!(!ptr.is_null());
+    assert!(ptr.is_aligned());
+
+    // When this is freed, GC assertions will check if the base pointer can be
+    // correctly recovered.
+    unsafe {
+        let _ = Box::from_raw_in(ptr, GcAllocator);
+    }
+
+    let y = Box::new_in(T(1, 2, 3, 4), GcAllocator);
+    let ptr = Box::into_raw(y);
+    assert!(!ptr.is_null());
+    assert!(ptr.is_aligned());
+
+    unsafe {
+        let _ = Box::from_raw_in(ptr, GcAllocator);
+    }
+}
+
+#[test]
+fn bdwgc_issue_589() {
+    // Test the specific size / alignment problem raised in [1].
+    //
+    // [1]: https://github.com/ivmai/bdwgc/issues/589
+    unsafe {
+        let allocator = GcAllocator;
+        let layout = Layout::from_size_align_unchecked(512, 64);
+        let raw_ptr = GlobalAlloc::alloc(&allocator, layout);
+        GlobalAlloc::dealloc(&allocator, raw_ptr, layout);
+    }
+}
